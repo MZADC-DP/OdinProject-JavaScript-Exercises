0a41419f5e8f28f073d7e75dc8266330b272a1dd
MZADC-DP <mzadc18@gmail.com>
2023-05-23 15:10:27 +0800

    Completed Exercises 1 & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
 
+    let words = "";
+
+    if (num < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < num; i++)
+        words += str;
+
+    return words;
 };
 
 // Do not edit below this line
