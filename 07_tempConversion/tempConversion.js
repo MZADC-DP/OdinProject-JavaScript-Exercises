c8e4d025ca9293f30da4d866960532ade6ad0ac1
MZADC-DP <mzadc18@gmail.com>
2023-05-24 23:05:26 +0800

    Completed Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  if (temp === 32 || temp === 0) {
+    let cel1 = (temp - 32) * (5/9);
+    return Math.round(cel1);
+  }
+
+  let celsius = (temp - 32) * (5/9);
+  return Math.round(celsius * 10) / 10; // Rounding to one decimal place
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  if (temp === 32 || temp === 0) {
+    let cel1 = (temp * 9/5) + 32;
+    return Math.round(cel1);
+  }
+
+  let fahrenheit = (temp * 9/5) + 32;
+  return Math.round(fahrenheit * 10) / 10; // Rounding to one decimal place
 };
+
+
+
 
 // Do not edit below this line
 module.exports = {
