7dd59656ef5b98518135858ce5496dd1ede21599
MZADC-DP <mzadc18@gmail.com>
2023-05-25 23:26:25 +0800

    Completed Exercise 8, 9 , 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const alphanumericStr = str.toLowerCase().replace(/[^a-z0-9]/g, ''); //Removes all special characters and numbers from the string
+    const reversedStr = alphanumericStr.split('').reverse().join('');    // Creates a reverse string of the original string
+  
+    return alphanumericStr === reversedStr;                              // Checks if they are equal
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+
+    let base = 0;
+    let arr = [1, 1];
+
+    if(num < 0 )
+        return 'OOPS';
+
+    for (let i = 0; i < num; i++)
+    {
+        arr[i + 2] = arr[i] + arr[i + 1];
+    }
+
+    return arr[num - 1];
+
+
 
 };
 
